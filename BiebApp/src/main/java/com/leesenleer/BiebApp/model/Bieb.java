eb014ea0854ee306b28fdd81130146f937164763
jap86 <jasper.mollema@gmail.com>
2019-02-08 15:54:59 +0100

    hij doet het, maar is leeg:)

--- BiebApp/src/main/java/com/leesenleer/BiebApp/model/Bieb.java
+++ BiebApp/src/main/java/com/leesenleer/BiebApp/model/Bieb.java
@@ -1,9 +1,6 @@
 package com.leesenleer.BiebApp.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
@@ -11,9 +8,46 @@ public class Bieb {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
 
-    int id;
-    String naam;
-    String beheerder;
-    String adres;
-    List<Boek> boeken;
+    private int id;
+    private String naam;
+    private String beheerder;
+    private String adres;
+    @OneToMany
+    private List<Boek> boeken;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getBeheerder() {
+        return beheerder;
+    }
+
+    public void setBeheerder(String beheerder) {
+        this.beheerder = beheerder;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public List<Boek> getBoeken() {
+        return boeken;
+    }
+
+    public void setBoeken(List<Boek> boeken) {
+        this.boeken = boeken;
+    }
 }
--- BiebApp/src/main/java/com/leesenleer/BiebApp/model/Boek.java
+++ BiebApp/src/main/java/com/leesenleer/BiebApp/model/Boek.java
@@ -9,9 +9,45 @@ import javax.persistence.Id;
 public class Boek {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    int id;
-    String naam;
-    String auteur;
-    int jaarVanUitgave;
-    int aantalBladzijden;
+    private int id;
+    private String naam;
+    private String auteur;
+    private int jaarVanUitgave;
+    private int aantalBladzijden;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+
+    public int getJaarVanUitgave() {
+        return jaarVanUitgave;
+    }
+
+    public void setJaarVanUitgave(int jaarVanUitgave) {
+        this.jaarVanUitgave = jaarVanUitgave;
+    }
+
+    public int getAantalBladzijden() {
+        return aantalBladzijden;
+    }
+
+    public void setAantalBladzijden(int aantalBladzijden) {
+        this.aantalBladzijden = aantalBladzijden;
+    }
 }
